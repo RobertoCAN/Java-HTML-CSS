1910733b1ba1765ea63dc1e074653987b7960259
RobertoCAN <94478699+RobertoCAN@users.noreply.github.com>
2023-07-07 13:55:57 -0300

    Add files via upload

--- hackerrank/src/hackerrank/CalculadoraTeste.java
+++ hackerrank/src/hackerrank/CalculadoraTeste.java
@@ -6,8 +6,14 @@ public class CalculadoraTeste {
 		int resultado = new Calculadora(new Soma()).calcular(10, 30);
 		System.out.println(resultado);
 		
-			resultado = new Calculadora(new Subtracao()).calcular(10, 30);
+		resultado = new Calculadora(new Subtracao()).calcular(10, 30);
 		System.out.println(resultado);
+		
+		resultado = new Calculadora(new Multiplicacao()).calcular(10, 30);
+		System.out.println(resultado);
+		
+		resultado = new Calculadora(new Divisao()).calcular(10, 30);
+		System.out.print(resultado);
 	}
 
 }
