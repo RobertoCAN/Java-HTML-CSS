5a9a8812b1ebb827282f570b6aa20fa609e4d4bf
RobertoCAN <94478699+RobertoCAN@users.noreply.github.com>
2024-01-11 16:35:34 -0300

    Alura
    Alteração para ENUM

---
+++ LojaAlura/src/br/com/alura/loja/TesteImposto.java
@@ -0,0 +1,16 @@
+package br.com.alura.loja;
+
+import java.math.BigDecimal;
+
+import br.com.alura.loja.imposto.CalculadoraDeImpostos;
+import br.com.alura.loja.imposto.TipoImposto;
+import br.com.alura.loja.orcamento.Orcamento;
+
+public class TesteImposto {
+	public static void main(String[] args) {
+		Orcamento orcamento = new Orcamento(new BigDecimal("100"));
+		CalculadoraDeImpostos calculadoraDeImpostos = new CalculadoraDeImpostos();
+		System.out.println(calculadoraDeImpostos.calcular(orcamento, TipoImposto.ICMS));
+		System.out.println(calculadoraDeImpostos.calcular(orcamento, TipoImposto.ISS));
+	}
+}
--- LojaAlura/src/br/com/alura/loja/imposto/CalculadoraDeImpostos.java
+++ LojaAlura/src/br/com/alura/loja/imposto/CalculadoraDeImpostos.java
@@ -1,12 +1,20 @@
 package br.com.alura.loja.imposto;
 
 import java.math.BigDecimal;
-
 import br.com.alura.loja.orcamento.Orcamento;
 
 public class CalculadoraDeImpostos {
 
-	public BigDecimal calcular(Orcamento orcamento) {
-		 return orcamento.getValor().multiply(new BigDecimal("0.1"));
+	public BigDecimal calcular(Orcamento orcamento, TipoImposto tipoIposto) {
+		switch (tipoIposto) {
+		case ICMS: {
+			return orcamento.getValor().multiply(new BigDecimal("0.1"));
+		}
+		case ISS: {
+			return orcamento.getValor().multiply(new BigDecimal("0.06"));
+		}
+		default:
+		return BigDecimal.ZERO;	
+		}
 	}
-}
+} 
