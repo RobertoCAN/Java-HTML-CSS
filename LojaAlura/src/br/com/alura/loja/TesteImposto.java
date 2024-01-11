594d26d267e39a56e3dc8afab8769e1ffa0b3277
RobertoCAN <94478699+RobertoCAN@users.noreply.github.com>
2024-01-11 17:02:53 -0300

    Strategy
    Alura alteração para Padrão comportamental strategy

--- LojaAlura/src/br/com/alura/loja/TesteImposto.java
+++ LojaAlura/src/br/com/alura/loja/TesteImposto.java
@@ -3,14 +3,15 @@ package br.com.alura.loja;
 import java.math.BigDecimal;
 
 import br.com.alura.loja.imposto.CalculadoraDeImpostos;
-import br.com.alura.loja.imposto.TipoImposto;
+import br.com.alura.loja.imposto.ICMS;
+import br.com.alura.loja.imposto.ISS;
 import br.com.alura.loja.orcamento.Orcamento;
 
 public class TesteImposto {
 	public static void main(String[] args) {
 		Orcamento orcamento = new Orcamento(new BigDecimal("100"));
 		CalculadoraDeImpostos calculadoraDeImpostos = new CalculadoraDeImpostos();
-		System.out.println(calculadoraDeImpostos.calcular(orcamento, TipoImposto.ICMS));
-		System.out.println(calculadoraDeImpostos.calcular(orcamento, TipoImposto.ISS));
+		System.out.println(calculadoraDeImpostos.calcular(orcamento, new ICMS()));
+		System.out.println(calculadoraDeImpostos.calcular(orcamento, new ISS()));
 	}
 }
