a8b947f9ffbdc63b7a710bbce4fae5f19becd209
RobertoCAN <94478699+RobertoCAN@users.noreply.github.com>
2024-01-22 16:59:01 -0300

    Botões
    alterando botões

--- -robotron-2000-projeto_inicial/js/main.js
+++ -robotron-2000-projeto_inicial/js/main.js
@@ -2,12 +2,21 @@ const controle = document.querySelectorAll(".controle-ajuste")
 
 controle.forEach((elemento)=>{
     elemento.addEventListener("click", (evento)=> {
-        manipulaDados(evento.target.textContent , evento.target.parentNode)
+       manipulaDados(evento.target.textContent , evento.target.parentNode)
     
     })
 })
 
-function manipulaDados(operacao, controle){
+function manipulaDados(operacao , controle){
+    const peca = controle.querySelector(".controle-contador")
+    if(operacao ==="-"){
+        peca.value = parseInt(peca.value) - 1
+    }else{
+        peca.value = parseInt(peca.value) + 1
+    }
+}
+
+/*function manipulaDados(operacao, controle){
     
     const peca = controle.querySelector(".controle-contador")
 
@@ -16,7 +25,7 @@ function manipulaDados(operacao, controle){
     }else{
         peca.value = parseInt(peca.value) + 1
     }
-}
+}*/
 
 robotron.addEventListener("click",  ()=>{
     console.log("Cliquei no robo")
