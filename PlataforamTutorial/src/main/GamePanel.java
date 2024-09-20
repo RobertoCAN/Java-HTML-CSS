bab5290258be1b8b53fae89271d82efe94828aa4
RobertoCAN <94478699+RobertoCAN@users.noreply.github.com>
2024-09-20 16:49:21 -0300

    Update
    controle do teclado.

--- PlataforamTutorial/src/main/GamePanel.java
+++ PlataforamTutorial/src/main/GamePanel.java
@@ -5,15 +5,18 @@ import java.awt.Graphics;
 
 import javax.swing.JPanel;
 
+import inputs.KeyboardInputs;
+
 public class GamePanel extends JPanel {
 
 	public GamePanel() {
-
+		addKeyListener(new KeyboardInputs());
 	}
 
 	public void paintComponent(Graphics g) {
 		super.paintComponent(g);
-		
-		g.drawRect(100, 100, 200, 50);
+		 
+		g.fillRect(100, 100, 200, 50);
 	}
+
 }
