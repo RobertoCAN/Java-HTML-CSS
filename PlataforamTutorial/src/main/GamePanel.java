5b0978a0cd4c374f17bf6098cf2f096d8297d9e4
RobertoCAN <94478699+RobertoCAN@users.noreply.github.com>
2025-03-06 16:12:24 -0300

    Mouse
    Colocando o mouse para funcionar

--- PlataforamTutorial/src/main/GamePanel.java
+++ PlataforamTutorial/src/main/GamePanel.java
@@ -6,11 +6,17 @@ import java.awt.Graphics;
 import javax.swing.JPanel;
 
 import inputs.KeyboardInputs;
+import inputs.MouseInputs;
 
 public class GamePanel extends JPanel {
+	
+	private MouseInputs mouseInputs;
 
 	public GamePanel() {
+		mouseInputs = new MouseInputs();
 		addKeyListener(new KeyboardInputs());
+		addMouseListener(mouseInputs);
+		addMouseMotionListener(mouseInputs);
 	}
 
 	public void paintComponent(Graphics g) {
