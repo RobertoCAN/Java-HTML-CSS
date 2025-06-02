b12d8de8ecc843f093ffadf0a0b85a96e7fc5291
RobertoCAN <94478699+RobertoCAN@users.noreply.github.com>
2025-06-02 16:22:57 -0300

    Encapsulamento
    Adcionando Funcionário e valor teste 1

---
+++ Emcapsulamento/src/funcionario/Funcionario.java
@@ -0,0 +1,51 @@
+package funcionario;
+
+public class Funcionario {
+
+	private String nome;
+	private String cargo;
+	private double salario;
+	private int controleReajuste = 0;
+
+	public Funcionario(String nome, double salario) {
+		this.nome = nome;
+		this.salario = salario;
+	}
+
+	
+	
+	public String getNome() {
+		return nome;
+	}
+
+
+
+	public String getCargo() {
+		return cargo;
+	}
+
+	public void setCargo(String cargo) {
+		this.cargo = cargo;
+	}
+
+	public double getSalario() {
+		return salario;
+	}
+	
+	public void reajustarSalario(double percentual) {
+		if (controleReajuste >= 2) {
+			System.out.println("\nNão pode fazer mais reajustes.");
+		}else {
+			controleReajuste++;
+			salario += salario * (percentual / 100);
+			System.out.printf("\nNovo salario de %s é %.2f ", nome, salario);
+		}
+	}
+	
+	public void exibirInformcoes() {
+		System.out.printf("\nFuncionario %s - Cargo: %s - Salário %.2f", nome, cargo, salario);
+	}
+	
+	
+	
+}
