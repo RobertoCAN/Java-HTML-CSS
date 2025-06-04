c591a1e20b55c2eed12b743fed7cb9e52cfbd732
Roberto Caetano Assunção Neto <robertofreeware@gmail.com>
2025-06-04 11:08:57 -0300

    Coloquei para funcionar adicionando adicionando no scanner e no switch usando o do while.

--- Emcapsulamento/src/funcionario/Funcionario.java
+++ Emcapsulamento/src/funcionario/Funcionario.java
@@ -4,7 +4,7 @@ package funcionario;
 public class Funcionario {
 
 	private String nome;
-	private String cargo;
+//	private String cargo;
 	private double salario;
 	private int controleReajuste = 0;
 
@@ -21,31 +21,31 @@ public class Funcionario {
 
 
 
-	public String getCargo() {
-		return cargo;
-	}
-
-	public void setCargo(String cargo) {
-		this.cargo = cargo;
-	}
+//	public String getCargo() {
+//		return cargo;
+//	}
+//
+//	public void setCargo(String cargo) {
+//		this.cargo = cargo;
+//	}
 
 	public double getSalario() {
 		return salario;
 	}
 	
-	public void reajustarSalario(double percentual) {
-		if (controleReajuste >= 2) {
-			System.out.println("\nNão pode fazer mais reajustes.");
-		}else {
-			controleReajuste++;
-			salario += salario * (percentual / 100);
-			System.out.printf("\nNovo salario de %s é %.2f ", nome, salario);
-		}
-	}
+//	public void reajustarSalario(double percentual) {
+//		if (controleReajuste >= 2) {
+//			System.out.println("\nNão pode fazer mais reajustes.");
+//		}else {
+//			controleReajuste++;
+//			salario += salario * (percentual / 100);
+//			System.out.printf("\nNovo salario de %s é %.2f ", nome, salario);
+//		}
+//	}
 	
-	public void exibirInformcoes() {
-		System.out.printf("\nFuncionario %s - Cargo: %s - Salário %.2f", nome, cargo, salario);
-	}
+//	public void exibirInformcoes() {
+//		System.out.printf("\nFuncionario %s - Cargo: %s - Salário %.2f", nome,  salario);
+//	}
 	
 	@Override
 	public String toString() {
--- Emcapsulamento/src/funcionario/TesteFuncionario.java
+++ Emcapsulamento/src/funcionario/TesteFuncionario.java
@@ -7,87 +7,55 @@ public class TesteFuncionario {
 
 	public static void main(String[] args) {
 
-//		ArrayList<Funcionario> funcionarios = new ArrayList<>();
-//
-//		Scanner scanner = new Scanner(System.in);
-//		int escolha;
+		Scanner scanner = new Scanner(System.in);
+		int escolha;
 
-
-		Funcionario funcionario = new Funcionario("Roberto", 15000);
+		Funcionario funcionario;
 		Funcionario funcionario2 = new Funcionario("caetano", 25000);
 		Funcionario funcionario3 = new Funcionario("Assunção", 35550);
 		Funcionario funcionario4 = new Funcionario("Neto", 45000);
-		
+
 		ArrayList<Funcionario> lista = new ArrayList<>();
-        lista.add(funcionario);
-        lista.add(funcionario2);
-        lista.add(funcionario3);
-        lista.add(funcionario4);
-        lista.forEach(System.out::println);
 
-//		do {
-//			// Exibe as opções do menu
-//			System.out.println("\n----- MENU -----");
-//			System.out.print("Escolha uma opção: \n");
-//			System.out.println("1. Para inserir o nome digite e cargo 1");
-//			System.out.println("2. Para inserir um cargo digite 2");
-//			System.out.println("0. Sair");
-//
-//			// Lê a escolha do usuário
-//			escolha = scanner.nextInt();
-//
-//			// Processa a escolha do usuário
-//			switch (escolha) {
-//			case 1:
-//				// Executa a ação da opção 1
-//				System.out.println("Insira o nome do funcionário");
-//				Scanner nome = new Scanner(System.in);
-//				String nomeFuncinario = nome.next();
-//
-//				System.out.println("Insira o salário do funcionário");
-//				Scanner salario = new Scanner(System.in);
-//				int salarioFuncinario = salario.nextInt();
-//
-//				funcionarios.add(new Funcionario(nomeFuncinario, salarioFuncinario));
-//
-//				break;
-//			case 2:
-//				// Executa a ação da opção 2
-//				System.out.println("Você escolheu a Opção 2");
-//				break;
-//			case 3:
-//				// Executa a ação da opção 3
-//				System.out.println("Você escolheu a Opção 3");
-//				break;
-//			case 0:
-//				// Sai do programa
-//				System.out.println("Saindo do programa.");
-//				break;
-//			default:
-//				// Opção inválida
-//				System.out.println("Opção inválida. Tente novamente.");
-//			}
-//		} while (escolha != 0);
-//
-//		// Fecha o Scanner
-//		scanner.close();
+		lista.add(funcionario2);
+		lista.add(funcionario3);
+		lista.add(funcionario4);
+		lista.forEach(System.out::println);
 
-		funcionario.setCargo("Analista");
+		do {
 
-		System.out.println("Funcionário tem o cargo: " + funcionario.getCargo());
-		System.out.println("Funcionário tem nome: " + funcionario.getNome());
-		System.out.println("Funcionário tem o salário: " + funcionario.getSalario());
+			System.out.println("\n----- MENU -----");
+			System.out.print("Escolha uma opção: \n");
+			System.out.println("1. Para inserir o nome digite e cargo 1");
+			System.out.println("2. Para inserir um cargo digite 2");
+			System.out.println("0. Sair");
 
-		funcionario.reajustarSalario(5);
-		funcionario.reajustarSalario(15);
-		funcionario.reajustarSalario(15);
-		funcionario.equals(funcionario);
-		
-//		for (int i = 0; i < funcionarios.size(); i++) {
-//			System.out.println("Funcionário tem o cargo: " + funcionario.getCargo());
-//			System.out.println("Funcionário tem nome: " + funcionario.getNome());
-//			System.out.println("Funcionário tem o salário: " + funcionario.getSalario());	
-//		}
+			escolha = scanner.nextInt();
+			switch (escolha) {
+			case 1:
+
+				System.out.println("Insira o nome do funcionário");
+				Scanner nome = new Scanner(System.in);
+				String nomeFuncinario = nome.next();
+
+				System.out.println("Insira o salário do funcionário");
+				Scanner salario = new Scanner(System.in);
+				double salarioFuncinario = salario.nextDouble();
+
+				funcionario = new Funcionario(nomeFuncinario, salarioFuncinario);
+				lista.add(funcionario);
+				break;
+			case 0:
+
+				System.out.println("Saindo do programa.");
+				break;
+			default:
+
+				System.out.println("Opção inválida. Tente novamente.");
+			}
+		} while (escolha != 0);
+
+		lista.forEach(System.out::println);
+
 	}
-
 }
