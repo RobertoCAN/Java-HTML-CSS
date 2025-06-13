e4eb858c20ee2e5173d6050a889623ad95e43a9b
Roberto Caetano Assunção Neto <robertofreeware@gmail.com>
2025-06-13 16:21:44 -0300

    fazendo o salvamento

--- Encapsulamento/src/usuario/Usuario.java
+++ Encapsulamento/src/usuario/Usuario.java
@@ -2,6 +2,27 @@ package usuario;
 
 public class Usuario {
 	private String nome;
+	private int pontos;
+	private int nivel;
+
+	public Usuario(String nome) {
+		this.nome = nome;
+		this.pontos = 0;
+		this.nivel = 1;
+	}
+
+	public void ganharPontos(int quantidade) {
+		this.pontos += quantidade;
+		atualizarNivel();
+	}
 	
+	private void atualizarNivel() {
+		this.nivel = (pontos / 100) + 1;
+	}
 	
+	public void exibirStatus() {
+		System.err.println("Nome: " + nome);
+		System.out.println("Pontos: " + pontos);
+		System.out.println("Nivel: " + nivel);
+	}
 }
