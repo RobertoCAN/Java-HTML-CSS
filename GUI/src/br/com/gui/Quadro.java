b66f2f3a42786815e36ba533ba64c8e3804df608
Roberto Caetano <robertofreeware@gmail.com>
2025-06-23 00:35:15 -0300

    Projetos Interfaces gráficas, testando

--- GUI/src/br/com/gui/Quadro.java
+++ GUI/src/br/com/gui/Quadro.java
@@ -1,11 +1,22 @@
 package br.com.gui;
 
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
 import javax.swing.JFrame;
 
 public class Quadro extends JFrame{
 
+	//page 22 pdf
+	
 	public Quadro() {
 		setTitle("Primeiro Frame!");
 		setSize(300, 200);
+		
+		addWindowListener(new WindowAdapter() {
+			public void windowClosing( WindowEvent e) {
+				System.exit(0);
+			}
+		});
 	}
 }
