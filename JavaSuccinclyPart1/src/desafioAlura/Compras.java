0f4903f89d7cacddcae9e7ad0228fcbd1ea589a4
Roberto Caetano Assunção Neto <robertofreeware@gmail.com>
2025-06-30 16:15:17 -0300

    colocando o desafio alura dentro do git em JavaSuccin

---
+++ JavaSuccinclyPart1/src/desafioAlura/Compras.java
@@ -0,0 +1,25 @@
+package desafioAlura;
+
+public class Compras {
+
+	private String descricao;
+	private double valor;
+
+	public Compras(String descricao, double valor) {
+		this.descricao = descricao;
+		this.valor = valor;
+	}
+
+	public String getProduto() {
+		return descricao;
+	}
+
+	public double getValor() {
+		return valor;
+	}
+
+	@Override
+	public String toString() {
+		return "Compra{" + "descricao= '" + descricao + '\'' + ", valor = " + valor + '}';
+	}
+}
