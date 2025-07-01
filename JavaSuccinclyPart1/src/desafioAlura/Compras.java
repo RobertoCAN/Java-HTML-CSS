dbc79086de3d92350e8634533940e51cd2684955
Roberto Caetano Assunção Neto <robertofreeware@gmail.com>
2025-07-01 15:09:48 -0300

    terminando o projeto do desafio.

--- JavaSuccinclyPart1/src/desafioAlura/Compras.java
+++ JavaSuccinclyPart1/src/desafioAlura/Compras.java
@@ -1,6 +1,6 @@
 package desafioAlura;
 
-public class Compras {
+public class Compras implements Comparable<Compras> {
 
 	private String descricao;
 	private double valor;
@@ -18,8 +18,19 @@ public class Compras {
 		return valor;
 	}
 
+	
+	public String getDescricao() {
+		return descricao;
+	}
+
 	@Override
 	public String toString() {
 		return "Compra{" + "descricao= '" + descricao + '\'' + ", valor = " + valor + '}';
+	}
+
+	@Override
+	public int compareTo(Compras o) {
+
+		return Double.valueOf(this.valor).compareTo(Double.valueOf(o.valor));
 	}
 }
