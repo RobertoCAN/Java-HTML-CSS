d448a0b28f705542992b3f480f07af20afc7c910
Roberto Caetano Assunção Neto <robertofreeware@gmail.com>
2025-07-04 16:22:18 -0300

    sei lá....

--- JavaSuccinclyPart1/src/javaApi/BuscaApi.java
+++ JavaSuccinclyPart1/src/javaApi/BuscaApi.java
@@ -5,18 +5,25 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.Scanner;
 
 public class BuscaApi {
 
 	public static void main(String[] args) throws IOException, InterruptedException {
+
+		Scanner leitura = new Scanner(System.in);
+		System.out.println("Digite um nome de filme.:");
+		var busca = leitura.nextLine(); 
+		
+		String endereco = "https://www.omdbapi.com/?t=" + busca + "&apikey=f8625842";
+		
 		HttpClient client = HttpClient.newHttpClient();
 
 		HttpRequest httpRequest = HttpRequest.newBuilder()
-				.uri(URI.create("https://www.omdbapi.com/?i=tt3896198&apikey=f8625842"))
+				.uri(URI.create(endereco))
 				.build();
 
-		HttpResponse<String> response = client
-				.send(httpRequest, HttpResponse.BodyHandlers.ofString());
+		HttpResponse<String> response = client.send(httpRequest, HttpResponse.BodyHandlers.ofString());
 
 		System.out.println(response.body());
 	}
