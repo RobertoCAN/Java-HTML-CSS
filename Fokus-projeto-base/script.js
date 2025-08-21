e83ccbd55aa2dc0e3e85bb73d6ff2b1dafa51168
Roberto Caetano Assunção Neto. <robertofreeware@gmail.com>
2025-08-21 15:07:01 -0300

    alterando, colocando o contador

--- Fokus-projeto-base/script.js
+++ Fokus-projeto-base/script.js
@@ -4,11 +4,16 @@ const curtoBT = document.querySelector('.app__card-button--curto')
 const longoBT = document.querySelector('.app__card-button--longo')
 const banner = document.querySelector('.app__image')
 const titulo = document.querySelector('.app__title')
+const startPauseBT = document.querySelector('#start-pause')
 //variavel para tirar o foco dos botões.
 const botoes = document.querySelectorAll('.app__card-button')
 //musica
 const musicaFocoInput = document.querySelector('#alternar-musica')
 const musica = new Audio('/sons/luna-rise-part-one.mp3')
+
+let tempoDecorridoEmSegundos = 5
+let intervaloId = null
+
 musica.loo = true
 //função para musica com função anonima
 musicaFocoInput.addEventListener('change', () =>{
@@ -66,4 +71,16 @@ function alterarContexto(contexto){
         default:
             break;
     }
+}
+
+const contagemRegressiva = () => {
+    //iniciar()
+    tempoDecorridoEmSegundos -= 1
+    console.log('Temporizador: '+ tempoDecorridoEmSegundos)
+}
+
+startPauseBT.addEventListener('click', contagemRegressiva)
+
+function iniciar(){
+    intervaloId = setInterval(contagemRegressiva, 1000)
 }
