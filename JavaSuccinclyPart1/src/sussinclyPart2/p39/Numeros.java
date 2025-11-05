2a68933848901cdbddcdf68b03b636e21b1c9785
Roberto Caetano Assunção Neto. <robertofreeware@gmail.com>
2025-11-05 16:28:49 -0300

    Pagina 42 Teste00

---
+++ JavaSuccinclyPart1/src/sussinclyPart2/p39/Numeros.java
@@ -0,0 +1,11 @@
+package sussinclyPart2.p39;
+
+public class Numeros {
+
+	double x;
+	
+	public Numeros(double x) {
+		this.x = x;
+		System.out.printf("%.0f",this.x);
+	}
+}
--- JavaSuccinclyPart1/src/sussinclyPart2/p39/Point.java
+++ JavaSuccinclyPart1/src/sussinclyPart2/p39/Point.java
@@ -4,20 +4,12 @@ public class Point implements Comparable {
 
 	public double x, y;
 
-	
-	
-	public double getX() {
-		return x;
-	}
-
-	public double getY() {
-		return y;
-	}
-
 	public Point(double x, double y) {
 
 		this.x = x;
 		this.y = y;
+
+		System.out.println(x + " " + y);
 	}
 
 	public void print() {
@@ -26,14 +18,14 @@ public class Point implements Comparable {
 
 	@Override
 	public int compareTo(Object o) {
-		if(!(o instanceof Point))
-		return 0;
-		
+		if (!(o instanceof Point))
+			return 0;
+
 		Point otherPoint = (Point) o;
-		
+
 		Double thisAbsMag = Math.sqrt(x * x + y * y);
 		double otherPointAbsmag = Math.sqrt(otherPoint.x * otherPoint.x + otherPoint.y * otherPoint.y);
-		
+
 		return thisAbsMag.compareTo(otherPointAbsmag);
 	}
 
