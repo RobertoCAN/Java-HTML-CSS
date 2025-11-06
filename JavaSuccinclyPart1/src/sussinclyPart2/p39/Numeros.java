cff3f149ee205ae63e084846af8529bf72e7fb17
Roberto Caetano Assunção Neto. <robertofreeware@gmail.com>
2025-11-06 16:40:08 -0300

    Erro testar

--- JavaSuccinclyPart1/src/sussinclyPart2/p39/Numeros.java
+++ JavaSuccinclyPart1/src/sussinclyPart2/p39/Numeros.java
@@ -1,11 +1,40 @@
 package sussinclyPart2.p39;
 
+import java.util.HashSet;
+import java.util.Random;
+import java.util.Set;
+
 public class Numeros {
 
+	Set<Integer> numerosUnicos = new HashSet<>();
+	Random random = new Random();
+	
 	double x;
 	
 	public Numeros(double x) {
 		this.x = x;
 		System.out.printf("%.0f",this.x);
 	}
+	
+	public void pegandoValorArray(int entrada) {
+		
+		while(numerosUnicos.size() < entrada) {
+			int numeroAleatorio = random.nextInt(50);
+			numerosUnicos.add(numeroAleatorio);
+		}
+		
+		Integer[] arrayFinal = new Integer[entrada];
+		int iniciando = 0;
+		
+		for (Integer numero : numerosUnicos) {
+			arrayFinal[iniciando++] = numerosUnicos;
+			
+		System.out.println("Impressão do array: ");
+		
+		for (int i : arrayFinal) {
+			System.out.println(i + " ");
+		}
+		}
+	}
+	
 }
