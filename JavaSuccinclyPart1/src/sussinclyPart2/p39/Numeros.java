e0a7907215071fb3c20223ad325365562437c94b
Roberto Caetano Assunção Neto. <robertofreeware@gmail.com>
2025-11-07 15:42:59 -0300

    Retirando o valor zero do array

--- JavaSuccinclyPart1/src/sussinclyPart2/p39/Numeros.java
+++ JavaSuccinclyPart1/src/sussinclyPart2/p39/Numeros.java
@@ -6,35 +6,38 @@ import java.util.Set;
 
 public class Numeros {
 
-	Set<Integer> numerosUnicos = new HashSet<>();
+	Set<Integer> numerosUnicos = new HashSet<Integer>();
+
 	Random random = new Random();
-	
+
 	double x;
-	
+
 	public Numeros(double x) {
 		this.x = x;
-		System.out.printf("%.0f",this.x);
+		System.out.printf("%.0f \n", this.x);
 	}
-	
+
 	public void pegandoValorArray(int entrada) {
-		
-		while(numerosUnicos.size() < entrada) {
-			int numeroAleatorio = random.nextInt(50);
-			numerosUnicos.add(numeroAleatorio);
+
+		while (numerosUnicos.size() < entrada) {
+			
+			int i = 0;
+			int numeroAleatorio = random.nextInt(25);
+			numerosUnicos.add(numeroAleatorio + 1);
+			if(numeroAleatorio == 14 ) System.out.println(numerosUnicos);
 		}
-		
+
 		Integer[] arrayFinal = new Integer[entrada];
 		int iniciando = 0;
-		
+
 		for (Integer numero : numerosUnicos) {
 			arrayFinal[iniciando++] = numero;
-			
-		System.out.println("Impressão do array: ");
-		
-		for (Integer i : arrayFinal) {
-			System.out.println(i + " ");
 		}
+		System.out.println("Impressão do array: ");
+
+		for (Integer i : arrayFinal) {
+			System.out.print(i + " ");
 		}
 	}
-	
+
 }
--- JavaSuccinclyPart1/src/sussinclyPart2/p39/TesteNumeros.java
+++ JavaSuccinclyPart1/src/sussinclyPart2/p39/TesteNumeros.java
@@ -3,10 +3,9 @@ package sussinclyPart2.p39;
 public class TesteNumeros {
 	public static void main(String[] args) {
 	
-		
 		Numeros numeros = new Numeros(Math.random()*25);
 	
-		numeros.pegandoValorArray(10);;
+		numeros.pegandoValorArray(15);
 		
 	}	
 	
