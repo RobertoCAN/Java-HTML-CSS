1129a7ee8637011b4d0edccd92fa67c99a037246
Roberto Caetano <robertofreeware@gmail.com>
2025-11-09 00:04:06 -0300

    Testetando o commit e o push do eclipse

--- GUI/src/br/com/gui2/Painel.java
+++ GUI/src/br/com/gui2/Painel.java
@@ -5,7 +5,7 @@ import java.awt.Graphics;
 import javax.swing.JPanel;
 
 public class Painel extends JPanel{
-
+//teste
 	public void paintComponent(Graphics g) {
 		super.paintComponent(g);
 		g.drawString("Bem vindo ao Swing", 100, 80);
